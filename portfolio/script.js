13c6ea7ed02a33572145e698de9fa8823860343f
Alexander Kozlov <alex-k@akozlov.lan>
2024-02-04 21:10:55 -0500

    added javascript smooth scroll and dark/light mode

---
+++ portfolio/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const themeToggleButton = document.getElementById('theme-toggle');
+
+themeToggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    
+});
